7735f437cb7f950136b5d12e88d44eca71f74869
Bruno Lollato <brunolollato@Brunos-Mac-Pro.local>
2023-12-14 11:12:40 -0300

    chore(commitlint and husky conifg): commitlint and husky config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,8 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+const { list } = require("./changelog.config.js");
+
+module.exports = {
+	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"type-enum": [2, "always", list],
+	},
+};
